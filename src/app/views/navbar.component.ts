3d217af6f601e52421deb0513297e835cee01610
LuLuMS0206 <luciamacho00@gmail.com>
2024-09-16 13:07:16 +0200

    add CV

--- src/app/views/navbar.component.ts
+++ src/app/views/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-navbar",
@@ -6,20 +6,12 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./navbar.component.scss"]
 })
 
-export class NavbarComponent implements OnInit {
-  
-  // clickButton: boolean = false;
-  // showText: boolean = false;
+export class NavbarComponent {
+  isMenuOpen = false; 
 
-  constructor() { 
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen; 
   }
 
-  ngOnInit() {
-
-  }
-
-  // mostrarTexto(){
-  //   this.showText = true;
-  // }
 }
