2faa5e96a17874a88bdbe23a04c4a689e381e998
LuLuMS0206 <luciamacho00@gmail.com>
2024-09-27 11:29:59 +0200

    change url video

--- src/app/views/projects.component.ts
+++ src/app/views/projects.component.ts
@@ -1,46 +1,24 @@
-import { Component, OnInit, HostListener, QueryList, ViewChildren, ElementRef } from "@angular/core";
+import { Component, AfterViewInit, QueryList, ViewChildren, ElementRef } from "@angular/core";
 
 @Component({
   selector: "app-projects",
   templateUrl: "./projects.component.html",
   styleUrls: ["./projects.component.scss"]
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements AfterViewInit {
   @ViewChildren('videoElement') videoElements!: QueryList<ElementRef<HTMLVideoElement>>;
 
   constructor() {}
 
-  ngOnInit() {
-    this.checkVisibility();
+  ngAfterViewInit() {
+    this.playAllVideos();
   }
 
-  @HostListener("window:scroll", [])
-  onWindowScroll() {
-    this.checkVisibility();
-  }
-
-  checkVisibility() {
-    const cards = document.querySelectorAll('.card');
-    const triggerPoint = window.innerHeight * 0.85;
-
-    cards.forEach((card, index) => {
-      const cardPosition = card.getBoundingClientRect().top;
-
-      if (cardPosition < triggerPoint) {
-        setTimeout(() => {
-          card.classList.add('visible');
-        }, index * 300);
-      }
+  playAllVideos() {
+    this.videoElements.forEach((video) => {
+      video.nativeElement.play().catch((error) => {
+        console.error("Error playing video:", error);
+      });
     });
-  }
-
-  playVideo(video: HTMLVideoElement) {
-    video.play();
-  }
-
-
-  pauseVideo(video: HTMLVideoElement) {
-    video.pause();
-    video.currentTime = 0; 
   }
 }
